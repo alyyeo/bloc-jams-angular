f8b0c58eb5ceb30ba517c31b54b65fc8a18797f0
Alyssa Yeo <alyssalyyeo@gmail.com>
2016-06-03 04:20:48 -0400

    used a service to load album data

--- app/scripts/controllers/AlbumCtrl.js
+++ app/scripts/controllers/AlbumCtrl.js
@@ -1,9 +1,9 @@
 (function() {
-	function AlbumCtrl() {
-		this.albumPicasso = angular.copy(albumPicasso);
+	function AlbumCtrl(Fixtures) {
+		this.albumPicasso = Fixtures.getAlbum();
 	}
 
 	angular
 		.module('blocJams')
-		.controller('AlbumCtrl', AlbumCtrl);
+		.controller('AlbumCtrl', ['Fixtures', AlbumCtrl]);
 })();
