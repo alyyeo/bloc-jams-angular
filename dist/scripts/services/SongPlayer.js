95817d3d1e4423ece3d1d7fb92053da9bfdb6b53
Alyssa Yeo <alyssalyyeo@gmail.com>
2016-06-10 17:47:52 -0400

    refactor and documentation

--- dist/scripts/services/SongPlayer.js
+++ dist/scripts/services/SongPlayer.js
@@ -2,6 +2,10 @@
 	function SongPlayer() {
 		var SongPlayer = {};
 
+		/**
+		* @desc currently playing song
+		* @type {Object}
+		*/
 		var currentSong = null;
 		/**
 		* @desc Buzz object audio file
@@ -28,11 +32,25 @@
 			currentSong = song;	
 		};
 
+		/**
+		* @function playSong
+		* @desc Plays current buzz object and sets playing to true
+		* @param {Object} song
+		*/
+		var playSong = function(song) {
+			currentBuzzObject.play();
+			song.playing = true;
+		};
+
+		/**
+		* @function play
+		* @desc Checks if currentSong is same as song just clicked on and plays song
+		* @param {Object} song
+		*/
 		SongPlayer.play = function(song) {
 			if (currentSong !== song){
 				setSong(song);
-				currentBuzzObject.play();
-				song.playing = true;
+				playSong(song);
 			} else if (currentSong === song) {
 				if (currentBuzzObject.isPaused()) {
 					currentBuzzObject.play();
@@ -40,6 +58,11 @@
 			}
 		};
 
+		/**
+		* @function pause
+		* @desc Pauses song
+		* @param {Object} song
+		*/
 		SongPlayer.pause = function(song) {
 			currentBuzzObject.pause();
 			song.playing = false;
