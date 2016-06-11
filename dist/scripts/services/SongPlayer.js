84ce08ee3a0c7c512f3f48697261319cb4360714
Alyssa Yeo <alyssalyyeo@gmail.com>
2016-06-10 23:37:58 -0400

    player bar info and next button

--- dist/scripts/services/SongPlayer.js
+++ dist/scripts/services/SongPlayer.js
@@ -21,8 +21,7 @@
 		*/
 		var setSong = function(song) {
 			if (currentBuzzObject) {
-				currentBuzzObject.stop();
-				SongPlayer.currentSong.playing = null;
+				stopSong(SongPlayer.currentSong);
 			}
 
 			currentBuzzObject = new buzz.sound(song.audioUrl, {
@@ -41,6 +40,16 @@
 		var playSong = function(song) {
 			currentBuzzObject.play();
 			song.playing = true;
+		};
+
+		/**
+		* @function stopStong
+		* @desc Stop playing song
+		* @param {Object} song
+		*/
+		var stopSong = function(song) {
+			currentBuzzObject.stop();
+			song.playing = null;
 		};
 
 		/**
@@ -96,8 +105,24 @@
 			currentSongIndex--;
 
 			if (currentSongIndex < 0) {
-				currentBuzzObject.stop();
-				SongPlayer.currentSong.playing = null;
+				stopSong(SongPlayer.currentSong);
+			} else {
+				var song = currentAlbum.songs[currentSongIndex];
+				setSong(song);
+				playSong(song);
+			}
+		};
+
+		/**
+		* @function next
+		* @desc Plays the next song
+		*/
+		SongPlayer.next = function() {
+			var currentSongIndex = getSongIndex(SongPlayer.currentSong);
+			currentSongIndex++;
+
+			if (currentSongIndex > currentAlbum.songs.size) {
+				stopSong(SongPlayer.currentSong);
 			} else {
 				var song = currentAlbum.songs[currentSongIndex];
 				setSong(song);
